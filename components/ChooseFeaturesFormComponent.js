a7f19ef8f1cf33d570e3d050e3632d7b997b04df
Bobby Keel <bobbykeel76@gmail.com>
2021-08-16 01:51:37 -0400

    choose features form

--- components/ChooseFeaturesFormComponent.js
+++ components/ChooseFeaturesFormComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { ImageBackgroundComponent } from 'react-native';
 import { ImageBackground } from 'react-native';
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker } from 'react-native';
 import { Card, Header, Icon, Button } from 'react-native-elements';
 
 class ChooseFeatures extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: 'notFamous',
+      gender: 'male',
+      ageRange: '10-19'
+    }
+  }
   static navigationOptions = {
     title: 'Choose features'
   }
@@ -18,13 +25,54 @@ class ChooseFeatures extends Component {
           <View style={styles.headerText}>
             <Text style={{ fontSize: 24 }}>Choose features</Text>
           </View>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Type</Text>
+            <Picker
+              style={styles.formItem}
+              selectedValue={this.state.type}
+              onValueChange={itemValue => this.setState({ type: itemValue })}
+            >
+              <Picker.Item label='Not Famous' value='notFamous' />
+              <Picker.Item label='Famous' value='famous' />
+              <Picker.Item label='Create Your Own' value='createYourOwn' />
+            </Picker>
+          </View>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Gender</Text>
+            <Picker
+              style={styles.formItem}
+              selectedValue={this.state.gender}
+              onValueChange={itemValue => this.setState({ gender: itemValue })}
+            >
+              <Picker.Item label='Male' value='male' />
+              <Picker.Item label='Female' value='female' />
+              <Picker.Item label='Androgyne' value='androgyne' />
+            </Picker>
+          </View>
+          <View style={styles.formRow}>
+            <Text style={styles.formLabel}>Age Range</Text>
+            <Picker
+              style={styles.formItem}
+              selectedValue={this.state.ageRange}
+              onValueChange={itemValue => this.setState({ ageRange: itemValue })}
+            >
+              <Picker.Item label='10 - 19' value='10-19' />
+              <Picker.Item label='20 - 29' value='20-29' />
+              <Picker.Item label='30 - 39' value='30-39' />
+              <Picker.Item label='40 - 49' value='40-49' />
+              <Picker.Item label='50 - 59' value='50-59' />
+              <Picker.Item label='59+' value='59+' />
+            </Picker>
+          </View>
           <View style={styles.buttonContainer}>
             <Button
               icon={
                 <Icon
-                name='transgender-alt'
-                type='font-awesome'
-                color='white'
+                  name='transgender-alt'
+                  type='font-awesome'
+                  color='white'
+                  size={30}
+                  style={{marginRight: 10}}
                 />
               }
               title='Generate profile'
@@ -32,13 +80,6 @@ class ChooseFeatures extends Component {
                 backgroundColor: '#2904ff'
               }}
               onPress={() => navigate('ProfileReady')}
-            />
-            <Button
-              title='Create your own'
-              buttonStyle={{
-                backgroundColor: '#2904ff'
-              }}
-              onPress={() => navigate('CreateChar')}
             />
           </View>
         </View>
@@ -52,16 +93,27 @@ const styles = StyleSheet.create({
     flex: 1
   },
   headerText: {
-    justifyContent: 'center', 
+    justifyContent: 'center',
     alignItems: 'center',
     flex: 0.3
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between', 
-    alignItems: 'center',
     margin: 10
+  },
+  formRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20
+  },
+  formLabel: {
+    fontSize: 18,
+    flex: 2
+  },
+  formItem: {
+    flex: 2
   }
 });
 
